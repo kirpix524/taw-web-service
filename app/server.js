cb48edaed6e8c04a315d41ef63640926a8daeeaf
surovcevnv <test8@test.ru>
2017-07-14 16:00:52 +0400

    Исправлен report

--- app/server.js
+++ app/server.js
@@ -39,6 +39,46 @@ router.get('/', function (req, res, next) {
 	res.send(strOut);
 });
 
+router.get('/excel', function (req, res, next) {
+	// Require library
+	var excel = require('excel4node');
+
+	// Create a new instance of a Workbook class
+	var workbook = new excel.Workbook();
+
+	// Add Worksheets to the workbook
+	var worksheet = workbook.addWorksheet('Sheet 1');
+	var worksheet2 = workbook.addWorksheet('Sheet 2');
+
+	// Create a reusable style
+	var style = workbook.createStyle({
+		font: {
+			color: '#FF0800',
+			size: 12
+		},
+		numberFormat: '$#,##0.00; ($#,##0.00); -'
+	});
+
+	// Set value of cell A1 to 100 as a number type styled with paramaters of style
+	worksheet.cell(1, 1).number(100).style(style);
+
+	// Set value of cell B1 to 300 as a number type styled with paramaters of style
+	worksheet.cell(1, 2).number(200).style(style);
+
+	// Set value of cell C1 to a formula styled with paramaters of style
+	worksheet.cell(1, 3).formula('A1 + B1').style(style);
+
+	// Set value of cell A2 to 'string' styled with paramaters of style
+	worksheet.cell(2, 1).string('string').style(style);
+
+	// Set value of cell A3 to true as a boolean type styled with paramaters of style but with an adjustment to the font size.
+	worksheet.cell(3, 1).bool(true).style(style).style({ font: { size: 14 } });
+
+	workbook.write('Excel.xlsx');
+
+	res.status(202).send('ok');
+})
+
 router.get('/report', function (req, res, next) {
 	// let path = require('path');
 	// res.sendFile(path.resolve("index.html"));
@@ -289,7 +329,7 @@ function getHTML(ostArray, movesArray) {
 	let html = '<html><head><title>Сервер торговля и склад</title><meta charset="UTF-8"><h1>Сервер торговля и склад</h1></head><body>';
 	html = html + '<h2>Текущие остатки</h2>';
 	for (let i = 0; i < ostArray.length; i++) {
-		html = html + movesArray[i] + '</br>';
+		html = html + ostArray[i] + '</br>';
 	}
 	html = html + '<h2>Моменты фиксации</h2>';
 	for (let i = 0; i < movesArray.length; i++) {
